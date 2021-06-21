d744f39fcc4be8818b15ecaabf4d960445cb0ad7
Aditya Wagh <55929413+adityawagh114@users.noreply.github.com>
2021-06-22 03:59:24 +0530

    Classes to store the parsed XML data.

--- classes.lisp
+++ classes.lisp
@@ -1,12 +1,19 @@
-;; attribute defines the name and the value  
+;; Here Classes are created to store the properties of the XML. Data of the parsed XML is stored here.
 
+;; attribute defines the name and the value  
 (defclass attribute ()
   ((name :accessor attribute-name)
-  (value :accessor attribute-value)))
+   (value :accessor attribute-value)))
   
-(defclass line()
-((mathData :accessor line-mathData)))
 
+
+;;   The line contains the mathData(various equations/definations) 
+(defclass line()
+  ((mathData :accessor line-mathData)))
+
+
+
+;; input has its attributes and lines. Dynamic arrays of both the properties are created. 
 (defclass input()
   ((attributes 
     :accessor input-attributes
@@ -17,6 +24,8 @@
     :initform (map-into (make-array 0 :fill-pointer 0 :adjustable t) (lambda () (make-instance 'line))))))
 
 
+
+;; editor has its attributes and lines. Dynamic arrays of both the properties are created.
 (defclass editor()
  ((attributes 
     :accessor editor-attributes
@@ -26,7 +35,9 @@
     :accessor editor-lines
     :initform (map-into (make-array 0 :fill-pointer 0 :adjustable t) (lambda () (make-instance 'line))))))    
 
- 
+
+
+;;   A cell contains attributes,editors as well as inputs. Dynamic arrays of the properties are created.
 (defclass cell()
   ((attributes 
     :accessor cell-attributes
@@ -41,6 +52,8 @@
     :initform (map-into (make-array 0 :fill-pointer 0 :adjustable t) (lambda () (make-instance 'input)))))) 
 
 
+
+;; The documnet acts as the root node of the XML. It conatins its attributes and cells. 
 (defclass document ()
   ((attributes
     :accessor document-attributes
