99ea6641e7f66a1f6b85b5692080ff0881f2b4e0
Aditya Wagh <55929413+adityawagh114@users.noreply.github.com>
2021-07-27 12:35:32 +0530

    Modified version created by merging wxmx2texi and  expressions_from_xml

---
+++ wxmx2texi1/expressions_from_xml.lisp
@@ -0,0 +1,33 @@
+;; expressions_from_xml.lisp -- construct Maxima expressions from XML
+;; copyright 2021 by Robert Dodier
+;; I release this work under terms of the GNU General Public License
+
+(ql:quickload :plump)
+
+(defun $expressions_from_xml (x)
+  (cond
+    ((plump:textual-node-p x)
+     (plump:text x))
+    ((plump:nesting-node-p x)
+     (let*
+       ((op (mfuncall '$parse_string (plump:tag-name x)))
+        (attribute-keys (loop for k being the hash-keys of (plump:attributes x) collect k))
+        (attribute-values (mapcar #'(lambda (a) (plump:get-attribute x a)) attribute-keys))
+        (children-list (coerce (plump:child-elements x) 'list))
+        (children-exprs (mapcar '$expressions_from_xml children-list)))
+       (if children-exprs
+         `((mqapply) ((,op) ,@attribute-values) ,@children-exprs)
+         `((mqapply) ((,op) ,@attribute-values) ,($expressions_from_xml (plump:first-child x))))))
+     (t ;; Not sure what to do here.
+       x)))
+
+  (defun $read_xml (f)
+  (plump:first-child (plump:parse (string f))))
+  
+   ;;pass string of <output> in f 
+
+;; (defun $read_xml (f)
+          
+
+  ;; (plump:parse (string f))
+  ;; )
